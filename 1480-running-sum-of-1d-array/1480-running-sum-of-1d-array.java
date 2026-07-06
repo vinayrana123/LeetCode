baf458557f4427dc4a14497b78aea1e43df1ed15
vinayrana123 <vinayrana38073@gmail.com>
2026-07-06 09:50:45 +0530

    Time: 0 ms (100%), Space: 44.5 MB (17.17%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -6,3 +6,17 @@ class Solution {
         return nums;
     }
 }
+
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+
+//         int[] ans = new int[nums.length];
+//         ans[0] = nums[0];
+
+//         for (int i = 1; i < nums.length; i++) {
+//             ans[i] = ans[i - 1] + nums[i];
+//         }
+
+//         return ans;
+//     }
+// }
