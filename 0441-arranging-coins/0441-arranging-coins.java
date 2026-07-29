c43d38090dacfa9c502b0b80e28731f77b56e438
vinayrana123 <vinayrana38073@gmail.com>
2026-07-29 14:00:31 +0530

    Time: 1 ms (100%), Space: 42.2 MB (98.14%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,27 +1,37 @@
-class Solution {
-    public int arrangeCoins(int n) {
-        int start=0;
-        int end = n;
+// class Solution {
+//     public int arrangeCoins(int n) {
+//         int start=0;
+//         int end = n;
 
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            long point = (long) mid * (mid+1)/2;
-            if(point>n){
-                end = mid-1;
-            }
-            else if(point<n){
-                start = mid+1;
-            }
-            else{
-                return mid;
-            }
-        }
-        return end;
+//         while(start<=end){
+//             int mid = start+(end-start)/2;
+//             long point = (long) mid * (mid+1)/2;
+//             if(point>n){
+//                 end = mid-1;
+//             }
+//             else if(point<n){
+//                 start = mid+1;
+//             }
+//             else{
+//                 return mid;
+//             }
+//         }
+//         return end;
+
+        //more optimize  
+        class Solution {
+        public int arrangeCoins(int n) {
+        return (int)(-0.5 + Math.sqrt(2L * n + 0.25));
+    }
+}
+
+        //brute force solution:-
+
         // int i=1;
         // while(n>=i){
         //     n=n-i;
         //     i++;
         // }
         //    return i-1;
-    }
-}
+//     }
+// }
