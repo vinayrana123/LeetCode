74b6b0ca36d91f4f7c6fbb3e5686d4a23ab387c1
vinayrana123 <vinayrana38073@gmail.com>
2026-08-03 20:43:49 +0530

    Time: 5 ms (19.7%), Space: 43 MB (73.65%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,18 +1,64 @@
+import java.util.Arrays;
+
 class Solution {
     public int specialArray(int[] nums) {
-       int n = nums.length;
-       for(int x =1;x<=n;x++){
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>= x){
-                count++;
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int start = 0;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Find first index where nums[index] >= mid
+            int left = 0;
+            int right = n - 1;
+            int first = n;
+
+            while (left <= right) {
+                int m = left + (right - left) / 2;
+
+                if (nums[m] >= mid) {
+                    first = m;
+                    right = m - 1;
+                } else {
+                    left = m + 1;
+                }
+            }
+
+            int count = n - first;
+
+            if (count == mid) {
+                return mid;
+            } 
+            else if (count > mid) {
+                start = mid + 1;
+            } 
+            else {
+                end = mid - 1;
             }
         }
-       
-       if(count==x){
-        return x;
-       }
-        }
+
         return -1;
     }
 }
+
+        //Brute Force
+
+//        int n = nums.length;
+//        for(int x =1;x<=n;x++){
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]>= x){
+//                 count++;
+//             }
+//         }
+       
+//        if(count==x){
+//         return x;
+//        }
+//         }
+//         return -1;
+//     }
+// }
