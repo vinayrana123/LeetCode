02cdc910f79bccacd19edcf0eaa1541050dc7b5a
vinayrana123 <vinayrana38073@gmail.com>
2026-08-05 22:20:05 +0530

    Time: 0 ms (100%), Space: 42 MB (55.7%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.java
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -1,21 +1,45 @@
 class Solution {
     public int reachNumber(int target) {
 
-        target = Math.abs(target);
+        target = Math.abs(target);   //absolute valuee hai abs ka mtlb
+        int start = 0;
+        int end = target;
 
-        int sum = 0;
-        int step = 0;
+        while(start<end){
+            int mid = start + (end-start)/2;
 
-        while (sum < target) {
-            step++;
-            sum += step;
+            long sum = (long) mid*(mid+1)/2;
+
+            if(sum>=target){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
         }
+//start  = minimum number of steps
+            long sum = (long) start * (start+1)/2;
+            while((sum-target)%2 != 0){
+                start++;
+                sum = start*(start+1)/2;
+            }
+            return (int) start;
 
-        while ((sum - target) % 2 != 0) {
-            step++;
-            sum += step;
-        }
-
-        return step;
+        
     }
 }
+//         int sum = 0;
+//         int step = 0;
+
+//         while (sum < target) {
+//             step++;
+//             sum += step;
+//         }
+
+//         while ((sum - target) % 2 != 0) {
+//             step++;
+//             sum += step;
+//         }
+
+//         return step;
+//     }
+// }
