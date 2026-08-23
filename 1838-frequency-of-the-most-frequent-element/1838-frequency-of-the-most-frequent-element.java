8a936f2520d462e8d22f115fe35fcb7e38e5d5d5
vinayrana123 <vinayrana38073@gmail.com>
2026-08-23 14:16:23 +0530

    Time: 33 ms (91.37%), Space: 94.9 MB (28.24%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,53 +1,77 @@
 class Solution {
 
-    public int bSearch(int targetIdx, int k, int[] nums, long[] prefixSum) {
+    // public int bSearch(int targetIdx, int k, int[] nums, long[] prefixSum) {
 
-        int target = nums[targetIdx];
+    //     int target = nums[targetIdx];
 
-        int l = 0;
-        int r = targetIdx;
+    //     int l = 0;
+    //     int r = targetIdx;
 
-        int bestIdx = targetIdx;
+    //     int bestIdx = targetIdx;
 
-        while (l <= r) {
+    //     while (l <= r) {
 
-            int mid = l + (r - l) / 2;
+    //         int mid = l + (r - l) / 2;
 
-            long count = targetIdx - mid + 1;
-            long windowSum = count * target;
+    //         long count = targetIdx - mid + 1;
+    //         long windowSum = count * target;
 
-            long currSum =
-                    prefixSum[targetIdx]
-                    - prefixSum[mid]
-                    + nums[mid];
+    //         long currSum =
+    //                 prefixSum[targetIdx]
+    //                 - prefixSum[mid]
+    //                 + nums[mid];
 
-            long ops = windowSum - currSum;
+    //         long ops = windowSum - currSum;
 
-            if (ops > k) {
-                l = mid + 1;
-            } else {
-                bestIdx = mid;
-                r = mid - 1;
-            }
-        }
+    //         if (ops > k) {
+    //             l = mid + 1;
+    //         } else {
+    //             bestIdx = mid;
+    //             r = mid - 1;
+    //         }
+    //     }
 
-        return targetIdx - bestIdx + 1;
-    }
+    //     return targetIdx - bestIdx + 1;
+    // }
     public int maxFrequency(int[] nums, int k) {
-        int n  = nums.length;
+    //     int n  = nums.length;
 
-        Arrays.sort(nums);
+    //     Arrays.sort(nums);
 
-        long[] prefixSum= new long[n];
+    //     long[] prefixSum= new long[n];
 
-        prefixSum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixSum[i]=prefixSum[i-1] + nums[i];
+    //     prefixSum[0]=nums[0];
+    //     for(int i=1;i<n;i++){
+    //         prefixSum[i]=prefixSum[i-1] + nums[i];
+    //     }
+    //     int result = 0;
+    //     for(int target_idx=0;target_idx<n;target_idx++){
+    //         result = Math.max(result,bSearch(target_idx,k,nums,prefixSum));
+    //     }
+    //     return result;
+
+
+    //slinding window
+
+    Arrays.sort(nums);
+
+    int n = nums.length;
+    int result =0;
+    int l=0;
+    long curSum = 0;
+    for(int r=0;r<n;r++){
+        
+        long target = nums[r];
+        curSum += nums[r];
+
+         while((r-l+1)*target - curSum>k){
+            curSum -= nums[l];
+            l++;
+         }
+         result = Math.max(result,r-l+1);
+
         }
-        int result = 0;
-        for(int target_idx=0;target_idx<n;target_idx++){
-            result = Math.max(result,bSearch(target_idx,k,nums,prefixSum));
-        }
-        return result;
+    return result;
+
     }
 }
