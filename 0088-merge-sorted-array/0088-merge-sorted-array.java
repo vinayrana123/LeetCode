d1b0371e3af5602dc2d7bc7a26a1aab0460ac97c
vinayrana123 <vinayrana38073@gmail.com>
2026-08-28 17:46:29 +0530

    Time: 0 ms (100%), Space: 43.7 MB (63.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,17 +1,47 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-           int[] result = new int[m + n];
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
 
-        for (int i = 0; i < nums1.length; i++) {
-        result[i] = nums1[i];
-    }
-
-        for (int i = 0; i < nums2.length; i++) {
-        result[m + i] = nums2[i];
-    } 
-    Arrays.sort(result);
-        for(int i=0;i<m+n;i++){
-            nums1[i]=result[i];
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
         }
     }
-} 
+}
+
+
+
+
+
+        //brute force approach
+
+        // isme jo 0 hai jo extra space hai bs bki kuchh nhi
+//            int[] result = new int[m + n];
+
+//         for (int i = 0; i < nums1.length; i++) {
+//         result[i] = nums1[i];
+//     }
+
+//         for (int i = 0; i < nums2.length; i++) {
+//         result[m + i] = nums2[i];
+//     } 
+//     Arrays.sort(result);
+//         for(int i=0;i<m+n;i++){
+//             nums1[i]=result[i];
+//         }
+//     }
+// } 
