97c6aeed877786264d321ed53f3ad6d5645091a7
vinayrana123 <vinayrana38073@gmail.com>
2026-08-31 17:35:44 +0530

    Time: 14 ms (92.17%), Space: 51.6 MB (37.21%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,21 +1,37 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-//brute force
         Arrays.sort(g);
         Arrays.sort(s);
         int n = g.length;
         int m = s.length;
-        int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                    if(s[j]>=g[i]){
-                        count++;
-                    
-                    s[j]  = -1;
-                    break;
-                    }
-                }
+        int i = 0;
+        int j=0;
+        while(i<n && j<m){
+            if(g[i]<=s[j]){
+                i++;
             }
-        return count;
+            j++;
+        }
+        return i;
     }
 }
+
+//brute force
+//         Arrays.sort(g);
+//         Arrays.sort(s);
+//         int n = g.length;
+//         int m = s.length;
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                     if(s[j]>=g[i]){
+//                         count++;
+                    
+//                     s[j]  = -1;
+//                     break;
+//                     }
+//                 }
+//             }
+//         return count;
+//     }
+// }
