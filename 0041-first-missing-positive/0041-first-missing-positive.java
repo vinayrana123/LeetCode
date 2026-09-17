b382670fb6ad14afd91a2a4a1697f9a945c05934
vinayrana123 <vinayrana38073@gmail.com>
2026-09-17 20:54:08 +0530

    Time: 1 ms (99.94%), Space: 71.2 MB (72.29%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]){
+                Swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+        for(int index=0;index<nums.length;index++){
+            if(nums[index] != index+1){
+                return index+1;
+            }
+        }
+        return nums.length+1;
+     }
+    static void Swap(int[] nums,int First,int Second){
+        int temp=nums[First];
+         nums[First] = nums[Second];
+         nums[Second] = temp;
+    }
+}
+  
