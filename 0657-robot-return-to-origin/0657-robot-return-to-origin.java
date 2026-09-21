99b33fa4fc8c615d9e19d7b14cd5ef22abcd277f
vinayrana123 <vinayrana38073@gmail.com>
2026-09-21 12:33:39 +0530

    Time: 4 ms (98.81%), Space: 45.1 MB (98.25%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x =0;
+        int y = 0;
+        for (char ch : moves.toCharArray()){
+            if(ch=='U')  y++;
+            else if(ch=='D')   y--;
+            else if(ch=='L')   x--;
+            else if(ch=='R')   x++;
+        }
+        return x == 0 && y == 0;
+    }
+}
